8ae09d6cd1de85b9cf4c0f94946f2e53921008b4
Aman Kumar Rai <tej171995@gmail.com>
2024-05-08 02:14:44 +0530

    Initial Commit

---
+++ Currency_Converter/Currency_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Currency_ConverterApp.swift
+//  Currency_Converter
+//
+//  Created by Aman on 08/05/24.
+//
+
+import SwiftUI
+
+@main
+struct Currency_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
